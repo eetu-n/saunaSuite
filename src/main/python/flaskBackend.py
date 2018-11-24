8d7403eb431cbbc8f745145f917aebc154a88576
rargh <eetu.t.narhi@gmail.com>
2018-11-24 17:29:09 +0200

    fixed typo

--- src/main/python/flaskBackend.py
+++ src/main/python/flaskBackend.py
@@ -7,7 +7,7 @@ apiCaller = VaisalaApi()
 @app.route('/')
 def index():
     temp = apiCaller.get_latest_device("Bench2").json()[0]["Measurements"]["Temperature"]["value"]
-    humi = apiCaller.get_latest_device("Bench2").json()[0]["Measurements"]["Relative Humidity"]["value"]
+    humi = apiCaller.get_latest_device("Bench2").json()[0]["Measurements"]["Relative humidity"]["value"]
     loylyThrown = 8
     logsUsed = 5
     return render_template('index.html', curTemp=temp, curHumi=humi, loyly=loylyThrown, logs=logsUsed)
