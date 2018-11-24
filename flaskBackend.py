b1b3bc1654476cc3f3bf17081333c663203186f3
rargh <eetu.t.narhi@gmail.com>
2018-11-24 17:13:41 +0200

    Implemented Flask functionality

---
+++ flaskBackend.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from src.api_class import VaisalaApi
+app = Flask(__name__)
+
+apiCaller = VaisalaApi()
+
+@app.route('/')
+def index():
+    temp = apiCaller.get_latest_device("Bench2").json()[0]["Measurements"]["Temperature"]["value"]
+    humi = apiCaller.get_latest_device("Bench2").json()[0]["Measurements"]["Relative Humidity"]["value"]
+    loylyThrown = 8
+    logsUsed = 5
+    return render_template('index.html', curTemp=temp, curHumi=humi, loyly=loylyThrown, logs=logsUsed)
+
+
+if __name__ == '__main__':
+    app.run()
